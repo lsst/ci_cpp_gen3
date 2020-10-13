a032d182f94b5ec4238288bbed13819a5d49735e
Christopher Waters <czw@princeton.edu>
2020-10-13 14:35:58 -0500

    loadLibraryEnvironment fix.
    The final component of the SIP fix is to ensure that commands are run
    using the lsst.sconsUtils.utils.libraryLoaderEnvironment, which
    prepends the necessary environment string to the commands.

--- ups/eupspkg.cfg.sh
+++ ups/eupspkg.cfg.sh
@@ -5,8 +5,3 @@ build() {
     export DYLD_LIBRARY_PATH=$LSST_LIBRARY_PATH
     python "$(command -v scons)" -j"$NJOBS" prefix="$PREFIX" version="$VERSION" cc="$CC"
 }
-
-install() {
-    clean_old_install
-    install_ups
-}
