c08a261fd6b09fe11edf1f4c4e7dacb2755b27d5
Eli Rykoff <erykoff@stanford.edu>
2024-08-15 14:09:11 -0700

    Update test_dark for new LSST isr task.

--- tests/test_dark.py
+++ tests/test_dark.py
@@ -37,8 +37,8 @@ LEGACY_MODE = os.environ.get("CI_CPP_LEGACY", "0")
 # TODO: DM-26396
 #       Update these tests to validate calibration construction.
 
-@unittest.skipUnless(LEGACY_MODE != "0", "Skipping legacy tests.")
-class DarkTestCasesLegacy(lsst.utils.tests.TestCase):
+@unittest.skipIf(LEGACY_MODE != "0", "Skipping new tests in legacy mode.")
+class DarkTestCases(lsst.utils.tests.TestCase):
     @classmethod
     def setUpClass(cls):
         """Setup butler and generate an ISR processed exposure.
@@ -53,48 +53,50 @@ class DarkTestCasesLegacy(lsst.utils.tests.TestCase):
         repoDir = os.path.join(getPackageDir("ci_cpp_gen3"), "DATA/")
         butler = dafButler.Butler(repoDir, collections=['LATISS/raw/all', 'calib/v00', 'LATISS/calib'])
 
-        config = ipIsr.IsrTaskConfig()
+        config = ipIsr.IsrTaskLSSTConfig()
+        config.doBias = True
+        config.expectWcs = False
+        config.doDefect = True
+        config.doDark = True
+        config.doFlat = False
+        config.doDiffNonLinearCorrection = False
+        config.doBootstrap = False
+        config.doDeferredCharge = False
+        config.doLinearize = True
+        config.doCorrectGains = False
+        config.doApplyGains = True
+        config.doVariance = True
         config.doSaturation = True
         config.doSuspect = True
-        config.doSetBadRegions = True
-        config.doOverscan = True
-        config.overscan.doParallelOverscan = True
-        config.overscan.fitType = 'MEDIAN_PER_ROW'
-        config.doBias = True
-        config.doVariance = True
-        config.doDark = True
-
-        config.doLinearize = False
-        config.doCrosstalk = False
+        config.doCrosstalk = True
         config.doWidenSaturationTrails = False
+        config.doInterpolate = True
+        config.doSetBadRegions = True
         config.doBrighterFatter = False
-        config.doDefect = True
-        config.doSaturationInterpolation = False
-        config.doStrayLight = False
-        config.doFlat = False
-        config.doApplyGains = False
-        config.doFringe = False
-        config.doMeasureBackground = False
-        config.doVignette = False
-        config.doAttachTransmissionCurve = False
-        config.doUseOpticsTransmission = False
-        config.doUseFilterTransmission = False
-        config.doUseSensorTransmission = False
-        config.doUseAtmosphereTransmission = False
 
-        isrTask = ipIsr.IsrTask(config=config)
-        rawDataId = {'detector': 0, 'exposure': 2021052500057, 'instrument': 'LATISS'}
+        isrTaskLSST = ipIsr.IsrTaskLSST(config=config)
+        rawDataId = {"detector": 0, "exposure": 2021052500057, "instrument": "LATISS"}
         # TODO: DM-26396
         # This is not an independent frame.
-        cls.raw = butler.get('raw', rawDataId)
-        cls.bias = butler.get('bias', rawDataId)
-        cls.dark = butler.get('dark', rawDataId)
-        cls.camera = butler.get('camera', rawDataId)
-        cls.defects = butler.get('defects', rawDataId)
+        cls.raw = butler.get("raw", dataId=rawDataId)
+        cls.bias = butler.get("bias", rawDataId)
+        cls.camera = butler.get("camera", rawDataId)
+        cls.ptc = butler.get("ptc", rawDataId)
+        cls.linearizer = butler.get("linearizer", rawDataId)
+        cls.crosstalk = butler.get("crosstalk", rawDataId)
+        cls.defects = butler.get("defects", rawDataId)
+        cls.dark = butler.get("dark", rawDataId)
 
-        results = isrTask.run(cls.raw, camera=cls.camera,
-                              bias=cls.bias, dark=cls.dark,
-                              defects=cls.defects)
+        results = isrTaskLSST.run(
+            cls.raw,
+            camera=cls.camera,
+            bias=cls.bias,
+            ptc=cls.ptc,
+            linearizer=cls.linearizer,
+            crosstalk=cls.crosstalk,
+            defects=cls.defects,
+            dark=cls.dark,
+        )
 
         cls.exposure = results.outputExposure
 
@@ -170,6 +172,68 @@ class DarkTestCasesLegacy(lsst.utils.tests.TestCase):
             # needs to be < 0.05
             fractionalError = np.abs(sigma - sigmaClip)/sigmaClip
             self.assertLess(fractionalError, 5.0, msg=f"Test 5.4: {amp.getName()} {fractionalError}")
+
+
+@unittest.skipUnless(LEGACY_MODE != "0", "Skipping legacy tests.")
+class DarkTestCasesLegacy(DarkTestCases):
+    @classmethod
+    def setUpClass(cls):
+        """Setup butler and generate an ISR processed exposure.
+
+        Notes
+        -----
+        DMTN-101 5.1:
+
+        Process an independent dark frame through the ISR including
+        overscan correction, bias subtraction, dark subtraction.
+        """
+        repoDir = os.path.join(getPackageDir("ci_cpp_gen3"), "DATA/")
+        butler = dafButler.Butler(repoDir, collections=['LATISS/raw/all', 'calib/v00', 'LATISS/calib'])
+
+        config = ipIsr.IsrTaskConfig()
+        config.doSaturation = True
+        config.doSuspect = True
+        config.doSetBadRegions = True
+        config.doOverscan = True
+        config.overscan.doParallelOverscan = True
+        config.overscan.fitType = 'MEDIAN_PER_ROW'
+        config.doBias = True
+        config.doVariance = True
+        config.doDark = True
+
+        config.doLinearize = False
+        config.doCrosstalk = False
+        config.doWidenSaturationTrails = False
+        config.doBrighterFatter = False
+        config.doDefect = True
+        config.doSaturationInterpolation = False
+        config.doStrayLight = False
+        config.doFlat = False
+        config.doApplyGains = False
+        config.doFringe = False
+        config.doMeasureBackground = False
+        config.doVignette = False
+        config.doAttachTransmissionCurve = False
+        config.doUseOpticsTransmission = False
+        config.doUseFilterTransmission = False
+        config.doUseSensorTransmission = False
+        config.doUseAtmosphereTransmission = False
+
+        isrTask = ipIsr.IsrTask(config=config)
+        rawDataId = {'detector': 0, 'exposure': 2021052500057, 'instrument': 'LATISS'}
+        # TODO: DM-26396
+        # This is not an independent frame.
+        cls.raw = butler.get('raw', rawDataId)
+        cls.bias = butler.get('bias', rawDataId)
+        cls.dark = butler.get('dark', rawDataId)
+        cls.camera = butler.get('camera', rawDataId)
+        cls.defects = butler.get('defects', rawDataId)
+
+        results = isrTask.run(cls.raw, camera=cls.camera,
+                              bias=cls.bias, dark=cls.dark,
+                              defects=cls.defects)
+
+        cls.exposure = results.outputExposure
 
 
 class MemoryTester(lsst.utils.tests.MemoryTestCase):
