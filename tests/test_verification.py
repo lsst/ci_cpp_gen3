c24c4dec1fe365e8a7cfae3cd7c9157bbbad845d
Eli Rykoff <erykoff@stanford.edu>
2024-11-13 07:17:59 -0800

    Tighten tolerances after config fixes for arch consistency.

--- tests/test_verification.py
+++ tests/test_verification.py
@@ -200,7 +200,7 @@ class VerificationTestCases(lsst.utils.tests.TestCase):
         mapping = {'run': ('verifyPtcStats', 'ptcRun.yaml'),
                    'det': ('verifyPtcDetStats', 'ptcDet.yaml')}
 
-        self.genericComparison('ci_cpv_ptc', dataId, mapping, delta=100.0)
+        self.genericComparison('ci_cpv_ptc', dataId, mapping, delta=50.0)
 
     def test_bfkVerify(self):
         """Run comparison for bfk.
@@ -225,7 +225,7 @@ class VerificationTestCases(lsst.utils.tests.TestCase):
         dataId = {"instrument": "LATISS", "detector": 0}
         mapping = {"run": ("verifyLinearizerStats", "linearizerRun.yaml"),
                    "det": ("verifyLinearizerDetStats", "linearizerDet.yaml")}
-        self.genericComparison("ci_cpv_linearizer", dataId, mapping, delta=300.0)
+        self.genericComparison("ci_cpv_linearizer", dataId, mapping, delta=2.0)
 
     @unittest.skipIf(LEGACY_MODE == 0, "Skipping crosstalk verify test.")
     def test_crosstalkVerify(self):
